cf79fd5b8656980747914ad5ceccd59ffb04f68e
pulsar21 <170107031@stu.sdu.edu.kz>
2021-07-25 21:25:52 +0500

    add require conf for db

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const pg = require('pg');
-pg.defaults.ssl = process.env.DATABASE_URL ? true : false;
 
 const proConfig = process.env.DATABASE_URL;
-const devConig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? proConfig : devConig, {
-    ssl: {
-        rejectUnauthorized: false
+const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? proConfig : devConfig, {
+    dialect: 'postgres',
+    ssl: true,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
 });
