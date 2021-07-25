8ae65a52ae637271bd6cbc363cc6268a939600c8
pulsar21 <170107031@stu.sdu.edu.kz>
2021-07-25 20:50:56 +0500

    add ssl for db

--- db.js
+++ db.js
@@ -3,4 +3,6 @@ const { Sequelize } = require('sequelize');
 
 const proConfig = process.env.DATABASE_URL;
 const devConig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? proConfig : devConig);
+module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? proConfig : devConig, {
+    ssl: true
+});
