67bb743ff9df67679dce79d71567fac6713ba959
pulsar21 <170107031@stu.sdu.edu.kz>
2021-07-25 21:38:42 +0500

    finally cfg for db

--- db.js
+++ db.js
@@ -3,13 +3,18 @@ const { Sequelize } = require('sequelize');
 
 const proConfig = process.env.DATABASE_URL;
 const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const sslOptions = {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false
+    }
+}
+const dialectOptions = process.env.NODE_ENV === 'production' ? sslOptions : {};
+
+console.log(dialectOptions);
+
 module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? proConfig : devConfig, {
     dialect: 'postgres',
     ssl: true,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
+    dialectOptions: dialectOptions
 });
