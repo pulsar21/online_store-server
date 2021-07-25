571cdb52902d5569a222869a309f0e3596e7f70e
pulsar21 <170107031@stu.sdu.edu.kz>
2021-07-25 21:14:53 +0500

    add rejectUnAuth for db configure

--- db.js
+++ db.js
@@ -6,5 +6,7 @@ pg.defaults.ssl = process.env.DATABASE_URL ? true : false;
 const proConfig = process.env.DATABASE_URL;
 const devConig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? proConfig : devConig, {
-    ssl: process.env.DATABASE_UR ? true : false
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
