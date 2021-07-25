2f635470cee9e2fb10c5f380ab50e84735896ce2
pulsar21 <170107031@stu.sdu.edu.kz>
2021-07-25 19:59:07 +0500

    add production cfg for db

--- db.js
+++ db.js
@@ -1,13 +1,6 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        ssl: true
-    }
-);
+const proConfig = process.env.DATABASE_URL;
+const devConig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? proConfig : devConig);
